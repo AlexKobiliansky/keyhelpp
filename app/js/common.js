b44884b68a599b4f222634159d55934dc500663e
alex <alex_kobilianskiy@bigmir.net>
2020-04-25 00:17:05 +0300

    section intro complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,65 @@
 $(document).ready(function(){
+    /**
+     * mobile-mnu customization
+     */
+    var mmenu = $('#mobile-mnu');
+    var menuLogo = mmenu.data("logo");
+    var $mmenu = mmenu.mmenu({
+        navbars: [{
+            content: [ "<img src=" + menuLogo + " class=\"img-responsive mm-logo\" alt=\"alt\"/>" ],
+        }],
+        "pageScroll": true,
+
+        "navbar": {
+            "title" : "",
+        },
+        "extensions": [
+            "theme-dark",
+            // "pagedim-black",
+            "position-front",
+            "fx-listitems-slide",
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-container"
+        },
+    });
+
+    var mmenuBtn = $("#mmenu-btn");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('is-active')
+    });
+
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "is-active" );
+        }, 300);
+    });
+    /**
+     * end mobile-mnu customization
+     */
+
+
+    /** FORMS START */
+    $.validate({
+        form : '.contact-form',
+        scrollToTopOnError: false
+    });
+
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $('input[type="checkbox"], select').styler();
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,10 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jquery-form-validator/form-validator/jquery.form-validator.min.js',
+		'app/libs/formstyler/jquery.formstyler.min.js',
+		'app/libs/inputmask/jquery.maskedinput.min.js',
+		'app/libs/mmenu/jquery.mmenu.all.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
